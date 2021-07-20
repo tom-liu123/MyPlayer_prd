afae113987de1e3a493dfcbc2cc09d0a333605f5
liu_cy <2338879935@qq.com>
2021-07-20 18:08:40 +0800

    完成设置部分的代码，未生效，包过期？

--- app/src/main/java/com/example/myplayer/ui/activity/SettingActivity.kt
+++ app/src/main/java/com/example/myplayer/ui/activity/SettingActivity.kt
@@ -1,10 +1,24 @@
 package com.example.myplayer.ui.activity
 
+import android.preference.PreferenceManager
+import androidx.appcompat.widget.Toolbar
 import com.example.myplayer.R
 import com.example.myplayer.base.BaseActivity
+import com.example.myplayer.util.ToolBarManager
+import org.jetbrains.anko.find
 
-class SettingActivity:BaseActivity() {
+class SettingActivity:BaseActivity(),ToolBarManager{
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
     override fun getLayoutId(): Int {
         return R.layout.activity_setting
+
+    }
+
+    override fun initData() {
+        initSettingToolbar()
+        //获取推送通知有没有选中
+        var sp = PreferenceManager.getDefaultSharedPreferences(this)
+        val push = sp.getBoolean("push",false)
+        println("push=$push")
     }
 }
--- app/src/main/java/com/example/myplayer/util/ToolBarManager.kt
+++ app/src/main/java/com/example/myplayer/util/ToolBarManager.kt
@@ -15,18 +15,30 @@ interface ToolBarManager {
     fun initMainToolBar(){
         toolbar.setTitle("一凡影音")
         toolbar.inflateMenu(R.menu.main)
+        //如果Java接口中只有一个未实现的方法  可以省略接口对象  直接用{}表示为实现的方法
+        toolbar.setOnMenuItemClickListener {
+            toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+            true
+        }
 
-        toolbar.setOnMenuItemClickListener(object :Toolbar.OnMenuItemClickListener{
-            override fun onMenuItemClick(item: MenuItem?): Boolean {
-                when(item?.itemId){
-                    R.id.setting->{
-                        //跳转到设置界面
-                        toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
-                    }
-                }
-                return true
-            }
+//        toolbar.setOnMenuItemClickListener(object :Toolbar.OnMenuItemClickListener{
+//            override fun onMenuItemClick(item: MenuItem?): Boolean {
+//                when(item?.itemId){
+//                    R.id.setting->{
+//                        //跳转到设置界面
+//                        toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+//                    }
+//                }
+//                return true
+//            }
+//
+//        })
+    }
 
-        })
+    /**
+     * 处理设置页面的呢toolbar
+     */
+    fun initSettingToolbar(){
+        toolbar.setTitle("设置界面")
     }
 }
