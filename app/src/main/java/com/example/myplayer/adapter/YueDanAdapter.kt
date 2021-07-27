f76f5f1c3e9d950b234074fc762d99e39671c0f5
liu_cy <2338879935@qq.com>
2021-07-27 12:13:11 +0800

    悦单界面数据出现

--- app/src/main/java/com/example/myplayer/adapter/YueDanAdapter.kt
+++ app/src/main/java/com/example/myplayer/adapter/YueDanAdapter.kt
@@ -17,8 +17,12 @@ class YueDanAdapter: RecyclerView.Adapter<YueDanAdapter.YueDanHolder>() {
     //更新的方法
     fun updateList(list:List<YueDanBean.PlayListsBean>?){
         list?.let {
+
+            println("list==============================>"+list)
             this.list.clear()
             this.list.addAll(list)
+
+            println("list==============================>"+list)
             /**
              * 把当前列表刷新一下
              */
@@ -36,6 +40,11 @@ class YueDanAdapter: RecyclerView.Adapter<YueDanAdapter.YueDanHolder>() {
     }
 
     override fun onBindViewHolder(holder: YueDanHolder, position: Int) {
+        var data1 = list.get(position)
+        //view
+        var itemView = holder?.itemView as YueDanItemView
+        //view+data
+        itemView.setData(data1)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/myplayer/widget/YueDanItemView.kt
+++ app/src/main/java/com/example/myplayer/widget/YueDanItemView.kt
@@ -5,11 +5,16 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
 import com.example.myplayer.R
+import com.itheima.player.model.bean.YueDanBean
+import kotlinx.android.synthetic.main.item_home.view.*
+import kotlinx.android.synthetic.main.item_yuedan.view.*
 
 /**
  * 悦单界面每个条目的自定义view
  */
 class YueDanItemView :RelativeLayout{
+
+
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -19,5 +24,17 @@ class YueDanItemView :RelativeLayout{
     )
     init {
         View.inflate(context, R.layout.item_yuedan,this)
+    }
+
+    /**
+     * 条目view控件的初始化
+     */
+    fun setData(data1: YueDanBean.PlayListsBean) {
+        //歌曲名称
+        title1.text = data1.comCode
+        //歌手名称
+        auto_name.text = data1.comName
+        //歌曲个数
+        count.text = data1.realTask.toString()
     }
 }
